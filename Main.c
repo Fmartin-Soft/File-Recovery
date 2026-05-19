9fe894dd3a6a0a91ac5233e11b7a92a23b3d4297
Fmartin-Soft <finley.martin-6@students.plymouth.ac.uk>
2026-05-19 08:07:59 +0100

    Made headway into the Main Code, Started the README, made a .gitignore

--- Main.c
+++ Main.c
@@ -1,6 +1,50 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <immintrin.h>
+
+
+
 int main();
+long int findSizeofDisk();
+void readFiles(diskSize, byteTemp);
+bool isSomething();
+
+bool isComplete = false;
+
+//struct to check for a beggining of a file
+struct checker
+{
+    
+};
+
 
 int main(){
 
+    long int diskSize = findSizeofDisk();
+    printf("We have %ld Bytes in the disk!\n", diskSize);
+
+    __uint8_t temp[512];
+
+    readFiles(diskSize, temp);
 
 };
+
+long int findSizeofDisk(){
+
+    FILE *card = fopen("/dev/sdb","rb");
+
+    fseek(card,0,SEEK_END);
+    long int diskSize = ftell(card);
+    fclose(card);
+    return diskSize;
+}
+
+void readFiles(size, byteTemp){
+    FILE *card = fopen("/dev/sdb","rb");
+
+    while (fread(byteTemp, 1, 512, card) == 512){ //while reading file from 1st byte to 512nd byte. also make sure were actually reading the 512nd byte
+        //TODO need to check. cant do a massive if else as thats ugly as anything
+        //use struct of values
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+### File Recovery C Project
+This project is something I am doing in C to test my capabilities in the langauge
+
+## Prerequesites
+Currently only (doesn't work) works in Linux, tested specifically in Kubuntu
